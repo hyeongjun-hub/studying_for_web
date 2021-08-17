f7fdb5be81b8b393d2153a653f7edb703bca15d6
Hyeongjun Jo <chogudwns@naver.com>
2021-08-17 20:46:56 +0900

    0817

--- selfStudy/event/event.js
+++ selfStudy/event/event.js
@@ -1,21 +1,69 @@
-const title = document.querySelector(".title-js");
+// const title = document.querySelector(".title-js");
+// const btn = document.querySelector("button");
 
-const COLOR_CN = "color",
-    WHITE_CN = "white";
+// const handlebtn = (e) => {
+//   console.log("hi");
+// };
 
-function clickhandler(){
-    if(!title.classList.contains(COLOR_CN)){
-        title.classList.add(COLOR_CN);
-        title.classList.add(WHITE_CN);
-    }
-    else{
-        title.classList.remove(COLOR_CN);
-        title.classList.remove(WHITE_CN);
-    }
+// btn.addEventListener("click", handlebtn);
+
+// const COLOR_CN = "color",
+//   WHITE_CN = "white";
+
+// function clickhandler() {
+//   if (!title.classList.contains(COLOR_CN)) {
+//     title.classList.add(COLOR_CN);
+//     title.classList.add(WHITE_CN);
+//   } else {
+//     title.classList.remove(COLOR_CN);
+//     title.classList.remove(WHITE_CN);
+//   }
+// }
+
+// function init() {
+//   title.addEventListener("click", clickhandler);
+// }
+
+// init();
+
+class ArrayUtilities {
+  /* Your magic here */
+  static addZeros = (arr) => {
+    return arr.map((number) => Number(number + "0"));
+  };
+  static moreThanFifty = (arr) => {
+    return arr.filter((number) => number > 50);
+  };
+  static removeFirst = (arr) => {
+    arr.shift();
+    return arr;
+  };
+  static sumAll = (arr) => {
+    const result = arr.reduce((suma, curVal) => {
+      return suma + curVal;
+    }, 0);
+    return result;
+  };
+
+  static divide = (num) => {
+    const arr = (num + "").split("");
+    return arr;
+  };
 }
 
-function init(){
-    title.addEventListener("click",clickhandler);
-}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-init();
+const addZeros = ArrayUtilities.addZeros(numbers);
+console.log(addZeros);
+
+const moreThanFifty = ArrayUtilities.moreThanFifty(addZeros);
+console.log(moreThanFifty);
+
+const noFirst = ArrayUtilities.removeFirst(moreThanFifty);
+console.log(noFirst);
+
+const sumAll = ArrayUtilities.sumAll(noFirst);
+console.log(sumAll);
+
+const divided = ArrayUtilities.divide(sumAll);
+console.log(divided);
